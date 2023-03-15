0ff2e1694a840dd223cbfeaf5b38522b3906fe22
Henley Kuang <henleyk@google.com>
2023-03-15 01:08:51 -0700

    Create skeleton of dmgformula package and include image of dmg formula

---
+++ dmgformula/dmgformula.go
@@ -0,0 +1,31 @@
+package dmgformula
+
+// PassiveAtkDmg computes the passive atk damage.
+func PassiveAtkDmg() int {
+	return 0
+}
+
+// PassiveAtkCritDmg computes the passive atk damage with crit.
+func PassiveAtkCritDmg() int {
+	return 0
+}
+
+// BasicAtkDmg computes the basic atk damage.
+func BasicAtkDmg() int {
+	return 0
+}
+
+// BasicAtkCritDmg computes the basic atk damage with crit.
+func BasicAtkCritDmg() int {
+	return 0
+}
+
+// SkillAtkDmg computes the skill atk damage.
+func SkillAtkDmg() int {
+	return 0
+}
+
+// SkillAtkCritDmg computes the skill atk damage with crit.
+func SkillAtkCritDmg() int {
+	return 0
+}
