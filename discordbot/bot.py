e73a2affef41cf1c185750eb3662b8fd8f553176
Henley Kuang <henleyk@google.com>
2023-03-17 00:58:31 -0700

    Add simple command to bot for testing

--- discordbot/bot.py
+++ discordbot/bot.py
@@ -2,13 +2,20 @@
 import os
 
 import discord
+from discord.ext import commands
 
 TOKEN = os.environ['DISCORD_TOKEN']
 
 client = discord.Client(intents=discord.Intents.default())
+client = commands.Bot(intents=discord.Intents.default(), command_prefix="!*")
+
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+
+@client.command()
+async def eval(ctx):
+  await ctx.send('Test!')
 
 client.run(TOKEN)
